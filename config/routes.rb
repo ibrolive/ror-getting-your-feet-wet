129f326a523244a2639c1d0411b0890f322a4348
Ibrahim Dauda <ibrolive@gmail.com>
2019-10-23 18:27:11 +0100

    updated routes information

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'article#index'
+  get 'article/index' => 'article#index'
+  get 'article/create' => 'article#create'
+  get 'article/read' => 'article#read'
+  get 'article/update' => 'article#update'
+  get 'article/destroy' => 'article#destroy'
+
+  match '*path' => redirect('/'), via: :get
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
